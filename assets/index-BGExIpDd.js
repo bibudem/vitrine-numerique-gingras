72749440d172fc54ee80078fe2e2fa27a5a4b3a5
Guillaume Tomasi <guillaume.tomasi@kffein.com>
2025-05-12 13:13:04 -0400

    new build

--- assets/index-BGExIpDd.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./app-BHllM0oC.js";import{o,Z as t,aA as r}from"./vendor-D1O0VZ-0.js";import"./@debug-lodash-BIjirobg.js";const a={name:"UiWysiwyg"},n={class:"UiWysiwyg wysiwyg"};function i(s,c,p,_,d,f){return o(),t("div",n,[r(s.$slots,"default")])}const u=e(a,[["render",i]]);export{u as default};
-//# sourceMappingURL=index-BGExIpDd.js.map
