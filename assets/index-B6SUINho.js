d650f3c562d815ff449986882c37626f4b16ca0d
Guillaume Tomasi <guillaume.tomasi@kffein.com>
2026-03-19 10:38:53 -0400

    [INSTALL ONLY] - less time in the "waiting screen" and start timer after intro (INSTALL=true set in index.html)

---
+++ assets/index-B6SUINho.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./app-Cg96HkSk.js";import{o,Z as t,aA as r}from"./vendor-BT8TDTHT.js";import"./@debug-lodash-BIjirobg.js";const a={name:"UiWysiwyg"},n={class:"UiWysiwyg wysiwyg"};function i(s,c,p,_,d,f){return o(),t("div",n,[r(s.$slots,"default")])}const u=e(a,[["render",i]]);export{u as default};
+//# sourceMappingURL=index-B6SUINho.js.map
